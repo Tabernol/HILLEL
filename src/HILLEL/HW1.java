82db4f52c14d26d9e67f2b47cf48375ffc391163
Maks_race <madventurerace@gmail.com>
2022-03-19 00:47:42 +0200

    1

--- src/HILLEL/HW1.java
+++ src/HILLEL/HW1.java
@@ -2,6 +2,8 @@ package HILLEL;
 
 public class HW1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+
     }
 }
