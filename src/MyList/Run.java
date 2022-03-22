42654242b4553fdcc54ae2a253301ec83a8aa981
Maks_race <madventurerace@gmail.com>
2022-03-22 03:09:09 +0200

    2

---
+++ src/MyList/Run.java
@@ -0,0 +1,27 @@
+package MyList;
+
+import MyList.Task1.AList;
+
+public class Run {
+    public static void main(String[] args) {
+        AList a = new AList();
+        a.add(3);
+        a.add(4);
+        a.addFirst(8);
+        a.add(4);
+        a.addFirst(8);
+        a.addFirst(9);
+        a.add(2);
+        a.add(2,46);
+        a.add(0,1);
+        a.add(16,555);
+
+
+
+
+        a.showArray();
+
+
+
+    }
+}
