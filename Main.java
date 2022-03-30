8ca6109fccd4ad14b657c0bf22a4504dc341aa82
Maks_race <madventurerace@gmail.com>
2022-03-31 02:02:03 +0300

    Task2 version 1.0

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 package src.HILLEL;
 
 import src.HILLEL.hw4.Car;
+import src.HILLEL.hw4.Task2;
 
 public class Main {
     public static void main(String[] args) {
-        Car accord = new Car("accord", 58, 2.4);
-        accord.iWantToDrive(100);
-        accord.iWantToDrive(150, 130);
-        accord.fillTank(15);
-        accord.showOdometer();
-        Car c = new Car();
-        c.iWantToDrive(100);
+//        Car accord = new Car("accord", 58, 2.4);
+//        accord.iWantToDrive(100);
+//        accord.iWantToDrive(150, 130);
+//        accord.fillTank(15);
+//        accord.showOdometer();
+//        Car c = new Car();
+//        c.iWantToDrive(100);
+        Task2 t = new Task2();
+        t.comparisonArrays();
     }
 }
