665f8fcdd444ae538beba6eabcedda57a7197b81
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2022-04-14 23:44:29 +0300

    hw9 create

--- hw9/FibonacciNumber.java
+++ hw9/FibonacciNumber.java
@@ -3,13 +3,4 @@ package hw9;
 import java.util.Arrays;
 
 public class FibonacciNumber {
-    public int fib(int n) {
-        int[] fibonacciArray = new int[n + 2];
-        fibonacciArray[0] = 0;
-        fibonacciArray[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
-        }
-        return fibonacciArray[n];
-    }
 }
